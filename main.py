df00f0e5d6d3dc26d5e69a134478d6efd0338ae0
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-04-10 19:25:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def help_command(update,context):
     update.message.reply_text("Search for it on Google, duh")
     
 def ccache_command(update,context):
-    clear_cache()
+    clearcache()
     update.message.reply_text("Done")
     
 def about_command(update,context):
+    update.message.reply_text("Chat ID: "+str(update.message.chat_id))
     update.message.reply_text("@TheSc1enceGuy(akshit singh) is the  developer of this bot... This bot will convert any sent font (.ttf or .otf) to a magisk flashable zip... enjoy!")
 
 def maker_command(update,context):
