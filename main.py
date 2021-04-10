21e2a762e260e6fbbd58b6a88e716ae0326e584c
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-04-10 16:39:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.document, ttfdownload))
     dp.add_error_handler(error)
     
-    updater.start_polling()
+    updater.start_polling(drop_pending_updates=True)
     updater.idle()
     
 tfonts = ['MiLanProVF.ttf',
