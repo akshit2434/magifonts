75ec71d3bc726123fe6047cd9c450a3a2d637197
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-04-13 09:01:32 +0530

    Update responses.py

--- responses.py
+++ responses.py
@@ -7,7 +7,7 @@ Created on Sat Apr 10 10:11:50 2021
 
 def sample_responses(input_text):
     user_message = str(input_text).lower()
-    if user_message in ("do you know anything?"):
-        return "i know nothing rn... but i am gonna get an update soon..."
+    if "hello" in user_message:
+        return "Hello Sar!"
     
     return ""
