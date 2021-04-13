c4dac198dbd1143c1d3d16aff59ed3cdc9dc5334
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-04-13 09:17:43 +0530

    Update responses.py
    adding some responses

--- responses.py
+++ responses.py
@@ -7,7 +7,13 @@ Created on Sat Apr 10 10:11:50 2021
 
 def sample_responses(input_text):
     user_message = str(input_text).lower()
-    if "hello" in user_message:
+    if ("hello" in user_message) or ("hey" in user_message) or ("hi" in user_message):
         return "Hello Sar!"
+    if "good morning" in user_message:
+        return "Good Morning Sar!"
+    if "good night" in user_message:
+        return "Good Night Sar!"
+    if "bye" in user_message:
+        return "See you soon Sar!"
     
     return ""
