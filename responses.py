4139fa7a9c31e2813bdf250e0e289a79193f4eb1
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-04-13 10:33:39 +0530

    Update responses.py

--- responses.py
+++ responses.py
@@ -17,7 +17,7 @@ def sample_responses(input_text):
             user_message = user_message.replace(ele, "") 
     
     message_array = user_message.split(" ")
-    if ("hello" in message_array) or ("hey" in message_array) or ("hi" in message_array):
+    if (("hello" in message_array) or ("hey" in message_array) or ("hi" in message_array)) and ("@" not in user_message):
         return "Hello Sar!"
     if "good morning" in user_message:
         return "Good Morning Sar!"
