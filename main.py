4d1183202f0f62c7c2ca3d7817cbf78dcdf0e2f4
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-04-14 14:37:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def owner_command(update,context):
     update.message.reply_text("@TheSc1enceGuy(akshit singh) is my father... and he is the owner too lel ;)")
     
 def handle_message(update,context):
+    return True
     #text = str(update.message.text).lower()
     #if r.sample_responses(text) not in (""):
     #    update.message.reply_text(r.sample_responses(text))
