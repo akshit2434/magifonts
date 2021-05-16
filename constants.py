2e12e75734ac13a112a1cbd249475bf134598743
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-05-16 12:21:51 +0530

    Update constants.py
    optimized...

--- constants.py
+++ constants.py
@@ -6,4 +6,7 @@ Created on Sat Apr 10 09:52:17 2021
 """
 
 API_KEY = "1751727989:AAGaftex7PLDiIbTwe5_izQMwYwGUS9p8FM"
+#API_KEY = "1774841533:AAGjeLwbTwQ-wqvXgUpW9EBIvq1zga6WDf0"
 base = "https://api.telegram.org/bot"+API_KEY+"/"
+gid = -1001393886080
+#gid = -1001370521379
