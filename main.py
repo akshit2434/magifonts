86e6516689723969c0a62d40353565d666396833
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-05-21 17:51:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -737,7 +737,7 @@ def listfiles(direc):
         return name.copy()
     
 def initialize():
-    #shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
+    constants.init()
     create_dir("todo")
     create_dir("magiFont")
     create_dir("ziptodo")
