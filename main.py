36e8da8d7dc682efa06a6c25e8f7487ed202243a
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-05-21 17:57:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -741,8 +741,8 @@ def initialize():
         shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
     except:
         print("7z already registered")
-    else:
-        shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
+    #else:
+        #shutil.register_unpack_format('7zip', ['.7z'], unpack_7zarchive)
     create_dir("todo")
     create_dir("magiFont")
     create_dir("ziptodo")
