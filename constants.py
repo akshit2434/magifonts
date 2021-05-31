ff4f45bb0c66ceb0ae36b8d143611ade4cff903f
akshit2434 <63487810+akshit2434@users.noreply.github.com>
2021-05-31 16:00:26 +0530

    Update constants.py
    new bot token

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ Created on Sat Apr 10 09:52:17 2021
 @author: akshit singh
 """
 
-API_KEY = "1751727989:AAGaftex7PLDiIbTwe5_izQMwYwGUS9p8FM"
+API_KEY = "1751727989:AAHiIUxnTXqp-uee-kbGmFHB-OGr5iaGryI"
 #API_KEY = "1774841533:AAGjeLwbTwQ-wqvXgUpW9EBIvq1zga6WDf0"
 base = "https://api.telegram.org/bot"+API_KEY+"/"
 gid = -1001393886080
